494203af80ef84726978f1d9238573a96b2b9a1f
Aiden Mitchell <aidenmitchell747@gmail.com>
2025-03-10 18:08:15 -0600

    Score

--- rpc.js
+++ rpc.js
@@ -1,6 +1,24 @@
+const score = {
+    wins: 0,
+    losses: 0,
+    ties: 0
+};
+
+function resetScore() {
+    score.wins = 0;
+    score.losses = 0;
+    score.ties = 0;
+    displayScore();
+}
+
+function displayScore() {
+    document.getElementById("score-display").innerText =
+    `Wins: ${score.wins} - Losses: ${score.losses} - Ties: ${score.ties}`;
+}
+
 function rpsGame(playerMove) {
     const randomNum = Math.random();
-
+    let computerMove = '';
     if (randomNum >= 0 && randomNum < 1/3) 
         computerMove = 'rock';
     else if (randomNum >= 1/3 && randomNum < 2/3)  
@@ -14,6 +32,7 @@ function rpsGame(playerMove) {
 
     if(computerMove === playerMove) {
         result = 'Tie';
+        score.ties++;
     }
     else if (
         (computerMove === 'paper' && playerMove === 'scissors') ||
@@ -21,10 +40,13 @@ function rpsGame(playerMove) {
         (computerMove === 'rock' && playerMove === 'paper')
     ) {
         result = 'Win';
+        score.wins++;
     }
     else {
-        result = 'loss'            
+        result = 'loss'
+        score.losses++;            
     }
     
     alert(`You picked ${playerMove}. Comp picked ${computerMove}. This is a ${result}`);
+    displayScore();
 }
